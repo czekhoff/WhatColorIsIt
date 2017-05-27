a8401ecf9e81509e590a82afaf089dee56b46147
czekhoff <me@czekhoff.com>
2017-05-27 23:03:06 +0300

    Swift 3

--- WhatColorIsIt/ViewController.swift
+++ WhatColorIsIt/ViewController.swift
@@ -14,17 +14,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var label: UILabel!
     
     func run() {
-        let calendar = NSCalendar.currentCalendar()
-        let hours = calendar.component(.Hour, fromDate: NSDate())
-        let minutes = calendar.component(.Minute, fromDate: NSDate())
-        let seconds = calendar.component(.Second, fromDate: NSDate())
+        let calendar = Calendar.current
+        let hours = (calendar as NSCalendar).component(.hour, from: Date())
+        let minutes = (calendar as NSCalendar).component(.minute, from: Date())
+        let seconds = (calendar as NSCalendar).component(.second, from: Date())
         
         let redColor = CGFloat(hours) / 255
         let greenColor = CGFloat(minutes) / 255
         let blueColor = CGFloat(seconds) / 255
         let color = UIColor(red: redColor, green: greenColor, blue: blueColor, alpha: 1)
         
-        label.textColor = UIColor.whiteColor()
+        label.textColor = UIColor.white
         label.text = "\(hours):\(minutes):\(seconds)"
     
         view.backgroundColor = color
@@ -33,12 +33,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: #selector(ViewController.run), userInfo: nil, repeats: true)
-        
-    }
-
-    override func preferredStatusBarStyle() -> UIStatusBarStyle {
-        return .LightContent
+        Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.run), userInfo: nil, repeats: true)
     }
 }
-
